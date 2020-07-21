14444c984b2778891d745697356fbdd448dc58e3
Joe-Thompson <bmo3696@gmail.com>
2020-07-21 13:14:49 -0400

    working on cors issues

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ Session = sessionmaker(bind=engine)
 session = Session()
 db = SQLAlchemy(server)
 Payload.max_decode_packets = 100000
-# socketio = SocketIO(server, cors_allowed_origins="*")
+socketio = SocketIO(server)
 # cors = CORS(server, resources={r"/*": {"origins": "*"}})
 # server.debug = True
 # server.host = 'localhost'
